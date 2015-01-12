1a2930901cd81802925693569a1026de2d519134
bbodi <bodi.balazs@multi-pass.hu>
2015-01-12 19:12:02 +0100

    offset for columns

--- src/main/kotlin/net/yested/bootstrap/common.kt
+++ src/main/kotlin/net/yested/bootstrap/common.kt
@@ -7,7 +7,13 @@ public enum class Device(val code:String) {
     LARGER : Device("lg")
 }
 
-public abstract data class DeviceSize(val size: Int, val device: Device)
+public abstract class ColumnModifier(val size: Int, val device: Device, val modifierString: String) {
+    override fun toString(): String = "col-${device.code}${modifierString}-${size}"
+}
+
+public abstract data class DeviceSize(size: Int, device: Device) : ColumnModifier(size, device, "")
+public data class Offset(size: Int, device: Device) : ColumnModifier(size, device, "-offset")
+
 public class ExtraSmall(size: Int): DeviceSize(size, Device.EXTRA_SMALL)
 public class Small(size: Int): DeviceSize(size, Device.SMALL)
 public class Medium(size: Int): DeviceSize(size, Device.MEDIUM)
