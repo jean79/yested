26f1f9aa13851bb25395390e79d0910ac249deec
jean79 <jan.kovar79@gmail.com>
2015-01-27 23:33:54 +0100

    Fixes for internet explorer

--- src/main/docsite/complex/masterdetails.kt
+++ src/main/docsite/complex/masterdetails.kt
@@ -20,6 +20,9 @@ import net.yested.bootstrap.Medium
 import net.yested.Fade
 import net.yested.bootstrap.FormStyle
 import net.yested.bootstrap.Small
+import net.yested.Component
+import kotlin.js.dom.html.HTMLElement
+import java.util.ArrayList
 
 enum class Continent(val label:String) {
     EUROPE : Continent("Europe");
@@ -30,112 +33,131 @@ enum class Continent(val label:String) {
 
 data class City(val name:String, val continent:Continent)
 
-class MasterDetail {
+class DetailScreen(
+        val editedCity:City?,
+        val saveHandler:(City)->Unit,
+        val cancelHandler:()->Unit) : Component {
 
-    val placeholder = Div()
+    val textInput = TextInput(placeholder = "City name")
+    val validator = Validator(inputElement = textInput, errorText = "Name is mandatory", validator = { it.size > 3})
+    val select = Select(data = Continent.values().toList(), renderer = { it.label })
 
-    val list = arrayListOf(
-                    City("Prague", Continent.EUROPE),
-                    City("London", Continent.EUROPE),
-                    City("New York", Continent.AMERICA))
-
-    val grid = Grid(columns = array(
-            Column(label = { +"City name"},
-                    render = { +it.name }, sortFunction = bootstrap.compareBy<City, String> { it.name },
-                    defaultSort = true),
-            Column(label = { +"Continent"},
-                    render = { +it.continent.label},
-                    sortFunction = bootstrap.compareBy<City, String> { it.continent.label }),
-            Column(label = { },
-                    render = { btsButton(size = ButtonSize.EXTRA_SMALL, label = { +"Edit" }, onclick = { edit(it) })},
-                    sortFunction = bootstrap.compareBy<City, String> { it.name }),
-            Column(label = { },
-                    render = { btsButton(size = ButtonSize.EXTRA_SMALL, look = ButtonLook.DANGER, label = { +"Delete" }, onclick = { delete(it) })},
-                    sortFunction = bootstrap.compareBy<City, String> { it.name })))
-
-    fun delete(city:City) {
-        list.remove(city)
-        grid.list = list
+    fun save() {
+        if (validator.isValid()) {
+            saveHandler(City(textInput.value, select.selectedItems.first()))
+        }
     }
 
-    fun edit(editedCity:City? = null) {
-
-        val textInput = TextInput(placeholder = "City name")
-        val validator = Validator(inputElement = textInput, errorText = "Name is mandatory", validator = { it.size > 3})
-        val select = Select<Continent>(renderer = { it.label })
-        select.data = Continent.values().toList()
-
-        fun close() {
-            placeholder.setChild( createMasterView(), Fade() )
-        }
-
-        fun save() {
-            if (validator.isValid()) {
-                if (editedCity != null) {
-                    list.remove(editedCity)
-                }
-                list.add(City(textInput.value, select.selectedItems.first()))
-                grid.list = list
-                close()
-            }
-        }
-
+    {
         if (editedCity != null) {
             textInput.value = editedCity.name
             select.selectedItems = listOf(editedCity.continent)
         }
-
-        placeholder.setChild(
-            Form(formStyle = FormStyle.HORIZONTAL, labelDef = Small(4), inputDef = Small(8)) with  {
-                item(label = { +"City name"}, validator = validator) {
-                    +textInput
-                }
-                item(label = { +"Continent" }) {
-                    +select
-                }
-                item(label = {}) {
-                    div {
-                        btsButton(label = { +"Save" }, look = ButtonLook.PRIMARY, type = ButtonType.SUBMIT, onclick = ::save)
-                        btsButton(label = { +"Cancel" }, onclick = ::close)
-                    }
-                }
-            }, Fade())
-
     }
 
-    fun createMasterView() =
-        div {
-            +grid
-            btsButton(label = { +"Add"}, onclick = { edit()})
-        }
-
-    fun createDiv(): Div {
-
-        placeholder.setChild(createMasterView(), Fade())
-        grid.list = list
-
-        return div {
-            row {
-                col(Medium(12)) {
-                    pageHeader { h3 { +"Master / Detail" } }
+    override val element: HTMLElement
+        get() = (Form(formStyle = FormStyle.HORIZONTAL, labelDef = Small(4), inputDef = Small(8)) with  {
+            item(label = { +"City name"}, validator = validator) {
+                +textInput
+            }
+            item(label = { +"Continent" }) {
+                +select
+            }
+            item(label = {}) {
+                div {
+                    btsButton(label = { +"Save" }, look = ButtonLook.PRIMARY, type = ButtonType.SUBMIT, onclick = { save() })
+                    btsButton(label = { +"Cancel" }, onclick = { cancelHandler() })
                 }
             }
-            row {
-                col(Medium(6)) {
-                    h4 { +"Demo" }
-                    +placeholder
-                }
-                col(Medium(6)) {
-                    h4 { +"Source code"}
-                    a(href = "https://github.com/jean79/yested/blob/master/src/main/docsite/complex/masterdetails.kt") {+"Source code is deployed on GitHub"}
-                }
-            }
-        }
-
-    }
+        }).element
 
 }
 
-fun masterDetail(): Div =
-    MasterDetail().createDiv()
+class MasterScreen(val list:ArrayList<City>, val editHandler:Function1<City?, Unit>) : Component {
 
+    val grid =
+            Grid(columns = array(
+                    Column(label = { +"City name"},
+                            render = { +it.name }, sortFunction = bootstrap.compareBy<City, String> { it.name },
+                            defaultSort = true),
+                    Column(label = { +"Continent"},
+                            render = { +it.continent.label},
+                            sortFunction = bootstrap.compareBy<City, String> { it.continent.label }),
+                    Column(label = { },
+                            render = { btsButton(size = ButtonSize.EXTRA_SMALL, label = { +"Edit" }, onclick = { editHandler(it) })},
+                            sortFunction = bootstrap.compareBy<City, String> { it.name }),
+                    Column(label = { },
+                            render = { btsButton(size = ButtonSize.EXTRA_SMALL, look = ButtonLook.DANGER, label = { +"Delete" }, onclick = { deleteCity(it) })},
+                            sortFunction = bootstrap.compareBy<City, String> { it.name })));
+
+    fun deleteCity(city: City) {
+        list.remove(city)
+        grid.list = list
+    }
+
+    {
+        grid.list = list
+    }
+
+    override val element: HTMLElement
+        get() = (Div() with {
+            +grid
+            btsButton(label = { +"Add" }, onclick = { editHandler(null) })
+        }).element
+
+}
+
+class MasterDetailDemo(): Component {
+
+    val placeholder = Div()
+
+    val list = arrayListOf(
+            City("Prague", Continent.EUROPE),
+            City("London", Continent.EUROPE),
+            City("New York", Continent.AMERICA))
+
+    fun saveCity(originalCity: City?, newCity: City) {
+        if (originalCity != null) {
+            list.remove(originalCity)
+        }
+        list.add(newCity)
+        displayMasterScreen()
+    }
+
+    fun editCity(city: City? = null) {
+        placeholder.setChild(DetailScreen(editedCity = city, saveHandler = { saveCity(city, it) }, cancelHandler = { displayMasterScreen() }), Fade())
+    }
+
+    fun displayMasterScreen() {
+        placeholder.setChild(MasterScreen(list, { editCity(it) }), Fade())
+    }
+
+    {
+        displayMasterScreen()
+    }
+
+    override val element: HTMLElement
+        get() = (Div() with {
+            +placeholder
+        }).element
+
+}
+
+fun masterDetail() =
+    div {
+        row {
+            col(Medium(12)) {
+                pageHeader { h3 { +"Master / Detail" } }
+            }
+        }
+        row {
+            col(Medium(6)) {
+                h4 { +"Demo" }
+                +MasterDetailDemo()
+            }
+            col(Medium(6)) {
+                h4 { +"Source code"}
+                a(href = "https://github.com/jean79/yested/blob/master/src/main/docsite/complex/masterdetails.kt") {+"Source code is deployed on GitHub"}
+            }
+        }
+    }
