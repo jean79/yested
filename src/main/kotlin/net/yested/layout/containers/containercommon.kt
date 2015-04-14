e05e0a7273d6af25432a68d5dd63c3be37e412bd
jean79 <jan.kovar79@gmail.com>
2015-04-14 22:54:29 +0200

    fixed imports

--- src/main/kotlin/net/yested/layout/containers/containercommon.kt
+++ src/main/kotlin/net/yested/layout/containers/containercommon.kt
@@ -1,10 +1,7 @@
 package net.yested.layout.containers
 
-import bootstrap.sample.test
 import net.yested.Dimension
 import net.yested.Div
-import net.yested.HTMLComponent
-import net.yested.with
 
 data class ContainerItem(val div: Div, val dimension: Dimension)
 
